a2f186630ef6ce41ed9b83b58fadef0ff27241ad
elDamba <108362137+elDamba@users.noreply.github.com>
2022-06-30 23:19:53 +0500

    Update №3.py

--- Project Euler/№3.py
+++ Project Euler/№3.py
@@ -1,9 +1,10 @@
-a = 13195
-for i in range(a-1, 1, -1):
-    b = False
-    if a % i == 0:
-        for k in range(i - 1, 1, -1):
-            if i % k == 0:
-                b = True
-        if b == True:
-            print(i, end=" ")
+a = 600851475143
+b = 2
+while 1:
+    if a % b == 0:
+        a /= b
+        if a == 1:
+            print(b)
+            break
+    b += 1
+    
