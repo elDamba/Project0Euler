f25e6013ed4d5d15a31374b653353dbacb96e7ea
elDamba <108362137+elDamba@users.noreply.github.com>
2022-07-13 23:50:42 +0500

    Add files via upload

---
+++ Project Euler/№9.py
@@ -0,0 +1,6 @@
+(a, b, c) = (0, 0, 0)
+for a in range(100, 332):
+    for b in range(a + 1, 500):
+        for c in range(a + 2, 500):
+            if a + b + c == 1000 and a**2 + b**2 == c**2:
+                print(a, b, c)
