2f1eb9b8f07d0e8c3e408762fd15d7f542c19fd7
elDamba <108362137+elDamba@users.noreply.github.com>
2022-07-14 11:06:20 +0500

    Update №9.py

--- Project Euler/№9.py
+++ Project Euler/№9.py
@@ -3,4 +3,4 @@ for a in range(100, 332):
     for b in range(a + 1, 500):
         for c in range(a + 2, 500):
             if a + b + c == 1000 and a**2 + b**2 == c**2:
-                print(a, b, c)
+                print(a * b * c)
